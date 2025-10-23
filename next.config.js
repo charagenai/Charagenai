4320195f54ef12e3906cc6b6d4e9ee7e5f37da77
charagenai <charagenai@gmail.com>
2025-10-23 18:58:12 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,7 @@
-module.exports={reactStrictMode:true};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+ experimental: {
+   appDir: true, // aktiviert das App Router-Verzeichnis
+ },
+};
+module.exports = nextConfig;
