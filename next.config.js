5bfb2ee50462011ccf05e464245e64a49cf17245
charagenai <charagenai@gmail.com>
2025-10-24 18:45:29 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const path = require('path');
 
-const nextConfig = {};
-module.exports = nextConfig;
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+      '@/lib': path.resolve(__dirname, './lib'),
+    };
+    return config;
+  },
+};
